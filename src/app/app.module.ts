c767c8796ceb712cb922f6514a4303096dc827ce
Apoquinto <RCLlanesA@gmail.com>
2022-01-31 22:51:16 -0600

    feat(App): Generate editable-table component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SelectableListComponent } from './components/selectable-list/selectable-list.component';
+import { EditableTableComponent } from './components/editable-table/editable-table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SelectableListComponent
+    SelectableListComponent,
+    EditableTableComponent
   ],
   imports: [
     BrowserModule,
